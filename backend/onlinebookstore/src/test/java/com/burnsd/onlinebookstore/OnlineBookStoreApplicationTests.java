29d48ee73d658c1311dd90a8fa8a4121f1eba7ab
BurnsD <Dustinburnsactech@gmail.com>
2022-08-22 14:22:32 -0500

    initializing java into project

---
+++ backend/onlinebookstore/src/test/java/com/burnsd/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burnsd.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
